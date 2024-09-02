19d1d6674f3a7ffe0a0402974993c52ded7c53d1
annagaom <122282732+annagaom@users.noreply.github.com>
2024-09-02 14:51:14 +0300

    Initial commit

---
+++ src/main/java/data/Laskin.java
@@ -0,0 +1,24 @@
+package data;
+
+public class Laskin
+{
+    public int summa(int eka, int toka)
+    {
+        return eka + toka;
+    }
+
+    public int erotus(int eka, int toka)
+    {
+        return eka - toka;
+    }
+
+    public int tulo(int eka, int toka)
+    {
+        return eka * toka;
+    }
+
+    public int osamaara(int eka, int toka)
+    {
+        return eka / toka;
+    }
+}
